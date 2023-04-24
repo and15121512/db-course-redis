9c8425d7f9ed811ceb9d16b77e885efc11b01142
sukharnikov.aa <andrew1512@mail.ru>
2023-04-24 23:05:50 +0300

    initial

---
+++ apps/app_str.py
@@ -0,0 +1,36 @@
+import redis
+import os
+import time
+import json
+
+# Waiting cluster setup
+time.sleep(10)
+
+redis_conn = os.environ['REDIS_NODE_1_RESOLVED']
+redis_host = redis_conn.split(':')[0]
+redis_port = redis_conn.split(':')[1]
+
+r = redis.RedisCluster(host=redis_host, port=int(redis_port))
+
+r.cluster_meet(redis_host, redis_port, target_nodes=redis.RedisCluster.ALL_NODES)
+
+print('Ping result: ' + str(r.ping(target_nodes=redis.RedisCluster.ALL_NODES)))
+
+with open('./data/data.json') as f:
+  arr = json.load(f)
+
+start_set = time.perf_counter()
+for i, elem in enumerate(arr):
+  r.set(str(i), json.dumps(elem))
+end_set = time.perf_counter()
+
+start_get = time.perf_counter()
+for i, elem in enumerate(arr):
+  r.get(str(i))
+end_get = time.perf_counter()
+
+print(f'SET time: {end_set - start_set:0.4f} seconds')
+print(f'GET time: {end_get - start_get:0.4f} seconds')
+
+print('Output example: '+str(r.get(str(1))))
+
